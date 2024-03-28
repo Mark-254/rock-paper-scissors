8ce9d8ca9150c9a45b93970ab58ffa71b56aea1d
Mark-254 <157490572+Mark-254@users.noreply.github.com>
2024-03-28 11:37:17 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const choices = ["rock","paper","scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+
+function playGame(playerChoice){
+
+const computerChoice = choices[Math.floor(Math.random()*3)];
+ let result = "";
+
+ if(playerChoice === computerChoice){
+    result = "ITS A TIE!"
+ }
+ else{
+    switch(playerChoice){
+        case "rock":
+        result = (computerChoice === "scissors") ? "YOU WIN" : "YOU LOSE";
+        break;
+        case "paper":
+      result = (computerChoice === "rock") ? "YOU WIN" : "YOU LOSE";
+         break;  
+         case "scissors":
+            result = (computerChoice === "paper") ? "YOU WIN" : "YOU LOSE";
+         break;
+    }
+
+ }
+ playerDisplay.textContent = 'PLAYER: $(playerChoice)';
+ computerDisplay.textContent = 'Computer:$(computerChoice)';
+resultDisplay.textContent = result;
+
+resultDisplay.classList.removeZ("greenText", "redText");
+
+switch(result){
+    case "YOU WIN":
+        resultDisplay.classList.add("greenText");
+        break;
+        case "YOU LOSE":
+            resultDisplay.classList.add("redText");
+            break;
+}
+
+
+
+}
